c2d728b7c67266e56f4a75c4e1847ad17d7cb014
ludoc0de <champi.ludo@tutanota.com>
2024-03-27 15:08:16 -0300

    Login component

---
+++ screens/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {
+  Alert,
+  StyleSheet,
+  ImageBackground,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+export default function Login() {
+  return <Text>Login</Text>;
+}
