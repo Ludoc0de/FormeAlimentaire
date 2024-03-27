271c18f507b3de306424f17c53d5abab30953b2e
ludoc0de <champi.ludo@tutanota.com>
2024-03-27 18:10:15 -0300

    create main

---
+++ screens/Main.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {
+  Alert,
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
+
+export default function Main() {
+  return <Text>Main</Text>;
+}
